56e862d78b1771c4d685d6bea8b024116a841fe0
Paulo Cesar <paulocezarcsn@gmail.com>
2020-12-08 22:13:02 -0300

    Adicionando o projeto

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:exercicio_final_lista_filmes/ui/tela_principal_visualizar_filmes.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: TelaVisualizarFilmes(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
